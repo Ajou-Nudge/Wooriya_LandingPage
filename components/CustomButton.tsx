445dab39753d42d64d21afd18443288ffcb7745e
JalJu <dlckswn334@naver.com>
2023-08-04 02:44:44 +0900

    event stopPropagation

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react';
 
 interface CustomButtonProps {
@@ -14,14 +13,15 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   ButtonContent,
   href
 }) => {
-  const handleButtonClick = () => {
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // 이벤트 버블링 막기
     window.location.href = href;
   };
 
   return (
-    <div className={`${ButtonStyle}`}>
+    <div className={ButtonStyle}>
       {/* 버튼 클릭 이벤트 추가 */}
-      <button className={`${FontStyle}`} onClick={handleButtonClick}>
+      <button className={FontStyle} onClick={handleButtonClick}>
         {ButtonContent}
       </button>
     </div>
